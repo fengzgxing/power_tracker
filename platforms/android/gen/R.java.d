23ae76bcb812502150cea736d55aef5acd167a69
fengzgxing <joe@cnksi.com>
2013-11-17 20:14:14 +0800

    add test pages

--- platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-D:\cnksi\power_tracker\platforms\android\gen\com\cnksi\powertracker\R.java \
- : D:\cnksi\power_tracker\platforms\android\res\drawable\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\drawable-hdpi\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\drawable-ldpi\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\drawable-mdpi\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\drawable-xhdpi\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\values\strings.xml \
-D:\cnksi\power_tracker\platforms\android\res\xml\config.xml \
-D:\cnksi\power_tracker\platforms\android\bin\AndroidManifest.xml \
