f5e6ffbdfc41c453dd1d59e3175a26b9a65d834f
fengzgxing <joe@cnksi.com>
2013-11-17 20:21:41 +0800

    add some sources

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+D:\cnksi\power_tracker\platforms\android\gen\com\example\hello\R.java \
+ : D:\cnksi\power_tracker\platforms\android\res\drawable\icon.png \
+D:\cnksi\power_tracker\platforms\android\res\drawable-hdpi\icon.png \
+D:\cnksi\power_tracker\platforms\android\res\drawable-ldpi\icon.png \
+D:\cnksi\power_tracker\platforms\android\res\drawable-mdpi\icon.png \
+D:\cnksi\power_tracker\platforms\android\res\drawable-xhdpi\icon.png \
+D:\cnksi\power_tracker\platforms\android\res\values\strings.xml \
+D:\cnksi\power_tracker\platforms\android\res\xml\config.xml \
+D:\cnksi\power_tracker\platforms\android\bin\AndroidManifest.xml \
--- platforms/android/gen/com/example/hello/R.java
+++ platforms/android/gen/com/example/hello/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.cnksi.powertracker;
+package com.example.hello;
 
 public final class R {
     public static final class attr {
