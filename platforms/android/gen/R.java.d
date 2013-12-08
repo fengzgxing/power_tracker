5f01d758bb8efdd0e44c1cf6cf28a4dff96aec07
fengzgxing <fengzgxing@163.com>
2013-12-08 17:51:25 +0800

    finish web sql operation

--- platforms/android/gen/R.java.d
+++ platforms/android/gen/R.java.d
@@ -1,9 +1,9 @@
-D:\cnksi\power_tracker\platforms\android\gen\com\example\hello\R.java \
- : D:\cnksi\power_tracker\platforms\android\res\drawable\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\drawable-hdpi\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\drawable-ldpi\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\drawable-mdpi\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\drawable-xhdpi\icon.png \
-D:\cnksi\power_tracker\platforms\android\res\values\strings.xml \
-D:\cnksi\power_tracker\platforms\android\res\xml\config.xml \
-D:\cnksi\power_tracker\platforms\android\bin\AndroidManifest.xml \
+D:\projects\android\power_tracker\platforms\android\gen\com\example\hello\R.java \
+ : D:\projects\android\power_tracker\platforms\android\res\drawable\icon.png \
+D:\projects\android\power_tracker\platforms\android\res\drawable-hdpi\icon.png \
+D:\projects\android\power_tracker\platforms\android\res\drawable-ldpi\icon.png \
+D:\projects\android\power_tracker\platforms\android\res\drawable-mdpi\icon.png \
+D:\projects\android\power_tracker\platforms\android\res\drawable-xhdpi\icon.png \
+D:\projects\android\power_tracker\platforms\android\res\values\strings.xml \
+D:\projects\android\power_tracker\platforms\android\res\xml\config.xml \
+D:\projects\android\power_tracker\platforms\android\bin\AndroidManifest.xml \
