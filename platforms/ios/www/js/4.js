4010f3bc96449d79a143be0c04962480884da9d5
joe <joe@joes-Mac-mini.local>
2013-12-29 11:28:57 +0800

    修改前台界面，取消设备列表界面，直接显示详细设备列表界面

--- platforms/ios/www/js/4.js
+++ platforms/ios/www/js/4.js
@@ -1,31 +1,38 @@
-
-//查询某个巡视点下的设备列表
-var queryDeviceSql = "select * from device where did in (select distinct(did) from card_content_ref where cardid = ? and iiid in (select iiid from inspection_point where barcode = ?) )";
-
-//巡视点下的设备列表
-var devicelist_of_point = {
+//查询某个巡视点、巡视设备 下的设备部件
+//var queryDevicePartListSql = "select * from device_part where dpid in (select distinct(dpid) from card_content_ref where cardid = ? and iiid in (select iiid from inspection_point where barcode = ? ));";
+var queryDevicePartListSql = "select dp.did,dp.name,dp.dpid,dr.irchunk,count(1) as dcount from device_part dp left join defect_record dr on dp.dpid = dr.dpid where dr.result_type is null and dp.dpid in(select dpid from card_content_ref where cardid = ? and iiid in (select iiid from inspection_point where barcode = ? ) ) group by dp.dpid";
+var devicepartlist={
     
     pageInit:function(){
-        $('#page_inspection_point_title').html(localStorage._pointname+' - 设备列表');
-        executeQuery(queryDeviceSql,[localStorage._cardid,localStorage._rfidCode],devicelist_of_point.dealQueryDeviceSqlResult);
+        $('#page_devicepart_title').html(localStorage._pointname+' - 巡视设备');
+        
+        executeQuery(queryDevicePartListSql,[localStorage._cardid,localStorage._rfidCode],devicepartlist.dealQueryDevicePartSqlResult);
     },
-    dealQueryDeviceSqlResult:function(tx,results){
+    dealQueryDevicePartSqlResult:function(tx,results){
         var lens = results.rows.length;
         if(lens>0){
-             var innerhtml = "";
-             for(var i=0;i<lens;i++){
-                 var __did = results.rows.item(i).did;
-                 var __dname = results.rows.item(i).name;
-                 innerhtml = innerhtml+'<li data-theme="c"><a href="#" data-transition="slide" onclick="devicelist_of_point.itemOnClick('+__did+',\''+__dname+'\')">'+__dname+'</a> </li>';
-             }
-             $('#devicelist').html(innerhtml);
+            var innerhtml = "";
+            for(var i=0;i<lens;i++){
+                //var __did = results.rows.item(i).did;
+                var __irchunk = results.rows.item(i).irchunk;
+                var __did = results.rows.item(i).did;
+                var __dpid = results.rows.item(i).dpid;
+                var __dpname = results.rows.item(i).name;
+                var __dcount = results.rows.item(i).dcount;
+                if(!__irchunk && __dcount==1){
+                    __dcount = 0;
+                }
+                innerhtml = innerhtml+'<li data-theme="c"><a href="#" data-transition="slide" onclick="devicepartlist.itemOnClick('+__did+','+__dpid+',\''+__dpname+'\')">'+__dpname+'</a><span class="ui-li-count jsp" style="color:red;">'+__dcount+'</span></li>';
+            }
+            $('#devicelist').html(innerhtml);
             $("#devicelist").listview("refresh");
         }else{
             kmsg('数据库中无设备数据，请在后台配置');
         }
     },
-    itemOnClick:function(did,dname){
+    itemOnClick:function(did,dpid,dname){
         localStorage._did = did;
+        localStorage._dpid = dpid;
         localStorage._dname = dname;
         $.mobile.changePage("5.html","slidedown",true,true);
     }
